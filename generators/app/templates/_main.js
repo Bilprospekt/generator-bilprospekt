3515160fb723c75004e41896b90e708048abced3
br0r <viktor@silfverstrom.com>
2015-05-11 10:30:32 +0200

    Updated main template with router

--- generators/app/templates/_main.js
+++ generators/app/templates/_main.js
@@ -3,3 +3,7 @@ var Dispatcher = require("dispatchers/Dispatcher");
 var <%= capitalName %>Store = require("./stores/<%= name %>_store");
 
 Dispatcher.registerStore(<%= capitalName %>Store);
+<% if(router) { %>
+//Router
+require('./routes/<%= name %>_router');
+<% } %>
